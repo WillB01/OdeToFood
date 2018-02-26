38285fbbc308afe52df37b0842f7b3e2dca9200e
WillB01 <wb.william@outlook.com>
2018-02-26 22:15:59 +0100

    adding stuff

---
+++ OdeToFood/Services/SqlResturantData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OdeToFood.Data;
+using OdeToFood.Models;
+
+namespace OdeToFood.Services
+{
+    public class SqlResturantData : IResturantData
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public SqlResturantData(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public Resturant Add(Resturant resturant)
+        {
+            _appDbContext.Add(resturant);
+            _appDbContext.SaveChanges();
+            return resturant;
+        }
+
+        public IEnumerable<Resturant> GetAll()
+        {
+            return _appDbContext.Resturants.OrderBy(r => r.Name);
+        }
+
+        public Resturant GetById(int id)
+        {
+            return _appDbContext.Resturants.FirstOrDefault(r => r.Id == id);
+
+        }
+    }
+}
