3052dbe39fcac9c97ff1e39d774d1383681df430
WillB01 <wb.william@outlook.com>
2018-02-26 13:19:50 +0100

    adding stuff

---
+++ OdeToFood/Services/InMemoryResturant.cs
@@ -0,0 +1,29 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Services
+{
+    public class InMemoryResturant : IResturantData
+    {
+        private readonly List<Resturant> _resturants;
+
+        public InMemoryResturant()
+        {
+            _resturants = new List<Resturant>()
+            {
+                new Resturant(){Id = 1, Name = "Pizzahut"},
+                new Resturant(){Id = 2, Name = "Burger King"},
+                new Resturant(){Id = 3, Name = "Vapiano"},
+                new Resturant(){Id = 4, Name = "China"},
+            };
+        }
+
+        public IEnumerable<Resturant> GetAll()
+        {
+            return _resturants.OrderBy(r => r.Name);
+        }
+    }
+}
