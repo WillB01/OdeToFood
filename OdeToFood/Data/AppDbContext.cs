a5f689186325673b8b926cdf09ae20f8cda5674e
WillB01 <wb.william@outlook.com>
2018-02-26 22:00:29 +0100

    Adding DbContext

---
+++ OdeToFood/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Resturant> Resturants { get; set; }
+    }
+}
