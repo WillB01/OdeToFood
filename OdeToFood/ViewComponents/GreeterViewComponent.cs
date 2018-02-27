e580d20b1bb0c09495c665ca4e638ec2955518c2
WillB01 <wb.william@outlook.com>
2018-02-27 12:13:00 +0100

    ViewComponent

---
+++ OdeToFood/ViewComponents/GreeterViewComponent.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.ViewComponents
+{
+    public class GreeterViewComponent : ViewComponent
+    {
+        private IResturantData _greeter;
+
+        public GreeterViewComponent(IResturantData greeter)
+        {
+            _greeter = greeter;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _greeter.GetAll();
+            return View("Default", model);
+        }
+    }
+
+    //public class GreeterViewComponent : ViewComponent
+    //{
+    //    private IGreeter _greeter;
+
+    //    public GreeterViewComponent(IGreeter greeter)
+    //    {
+    //        _greeter = greeter;
+    //    }
+
+    //    public IViewComponentResult Invoke()
+    //    {
+    //        var model = _greeter.Message;
+    //        return View("Default",model);
+    //    }
+    //}
+}
